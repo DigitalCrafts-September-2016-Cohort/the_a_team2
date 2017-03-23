ac744e697a569549c2ee0c506529734a1f22155e
Keyur Patel <ekeyur@gmail.com>
2017-03-23 18:56:49 -0400

    added 2 way graphs for all the vertical dots in terminals

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ var concourses = [  {'name':'T', 'longitude':'-84.442447'},
 
 var E_terminal_horizontal_gates = ["E14","E15","E16","E17","E18"];
 
+// Cloning the origin points array
 var clone = JSON.parse( JSON.stringify( poi ));
 
+// Function to add a 2 way node
 function add2WayNode(pointA,pointB,distance){
   g.addNode(pointA,{[pointB] : distance});
   g.addNode(pointB,{[pointA] : distance});
 }
 
+// Define a point
 function Point(id,name,type,concourse,latitude,longitude){
   this.id = id;
   this.name = name;
@@ -65,18 +68,22 @@ concourses.forEach(function(concourse){
   // Loop through each gate/restaurat point
   for(let i=0;i<clone.length;i++){
     // If the point exist in the horizontal part of E terminal, then bool1 will be true skip it.
-    let bool1 = E_terminal_horizontal_gates.contains(clone[i].name)
+    let bool1 = E_terminal_horizontal_gates.includes(clone[i].name)
     // Skip, if the point_type = 'train';
     let bool2 = clone[i].poi_type === 'train';
     // Checking if the concourse name in points array is same as the concourse name in concourses database & previous two steps
       if(clone[i].concourse === concourse.name && !bool1 && !bool2){
         // Create a point with using the Point class/function
-        let dummy_vertical_point = new Point(point_id.toString(),concourse.name+gate.name,"vcenter",clone[i].concourse,clone[i].latitude,concourse.longitude);
+        let dummy_vertical_point = new Point(point_id.toString(),concourse.name+clone[i].name,"vcenter",clone[i].concourse,clone[i].latitude,concourse.longitude);
         // push the point in the poi database
         poi.push(dummy_vertical_point);
-        // 
+        // add a two way node between the newly created point and a gate/restaurat point in the graph.
         add2WayNode(point_id.toString(),clone[i].id,46)
       }
+    // Incrementing the point
     point_id += 1;
   }
 });
+
+// Cloning the array again
+var clone2 = JSON.parse( JSON.stringify( poi ));
